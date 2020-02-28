b70b8dda2fc3ab4ee4a61c16be5bda85a074b9b0
Loil Smith <51259796+SterbenLoil@users.noreply.github.com>
2020-02-28 14:47:25 +0700

    Changes

--- script.js
+++ script.js
@@ -931,35 +931,5 @@ $(function()
         
     }
 
-		
-//length: how many bars
-//height: height of the tallest bar
-//nowselected: which bar is selected
-function drawvolumecontroller(length,height,nowselected){    
-    document.getElementById("volumcontroller").innerHTML = "";
-    for (i=0;i<length;i++){
-        magassag = 7 + Math.round((1.4)*(length - i)); 
-        margintop = height-magassag;
-        if (margintop <= 0) {margintop=0;}
-        if (i >= nowselected){        
-            document.getElementById("volumcontroller").innerHTML = 
-            document.getElementById("volumcontroller").innerHTML + 
-            '<div  onmouseup="volumecontrolchanged(' + i + 
-            ')" style="background-color:#898989;height:' + magassag + 
-            'px;margin-top:'+margintop+'px;" class="volumecontrollerbar"></div>';
-        } else {
-            document.getElementById("volumcontroller").innerHTML = 
-            document.getElementById("volumcontroller").innerHTML + 
-            '<div  onmouseup="volumecontrolchanged(' + i + 
-            ')" style="height:'+magassag+'px;margin-top:' + margintop + 
-            'px;"class="volumecontrollerbar"></div>';
-        }        
-    }    
-}
-function volumecontrolchanged(newvolume){
-    drawvolumecontroller(20,35,newvolume);
-    document.getElementById("volumeindicator").innerHTML = newvolume;
-}
-    
 	initPlayer();
 });
